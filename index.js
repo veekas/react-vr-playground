3c106180f06ab9a68e57d01fc43defb99489fc5d
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-12-18 03:42:06 -0500

    lint and remove unneeded files

--- index.js
+++ index.js
@@ -1,155 +1,43 @@
-// import React from 'react';
-// import {
-//   AppRegistry,
-//   StyleSheet,
-//   Text,
-//   View,
-// } from 'react-360';
-
-// export default class react_vr_playground extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.panel}>
-//         <View style={styles.greetingBox}>
-//           <Text style={styles.greeting}>
-//             Welcome to React 360
-//           </Text>
-//         </View>
-//       </View>
-//     );
-//   }
-// };
-
-// const styles = StyleSheet.create({
-//   panel: {
-//     // Fill the entire surface
-//     width: 1000,
-//     height: 600,
-//     backgroundColor: 'rgba(255, 255, 255, 0.4)',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   greetingBox: {
-//     padding: 20,
-//     backgroundColor: '#000000',
-//     borderColor: '#639dda',
-//     borderWidth: 2,
-//   },
-//   greeting: {
-//     fontSize: 30,
-//   },
-// });
-
-// AppRegistry.registerComponent('react_vr_playground', () => react_vr_playground);
-
 import React from 'react';
 import {
   AppRegistry,
-  Environment,
   StyleSheet,
   Text,
   View,
-  VrButton,
 } from 'react-360';
 
-class Background extends React.Component {
-  constructor(props) {
-    super();
-    Environment.setBackgroundImage(props.uri, {format: props.format});
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (
-      nextProps.uri !== this.props.uri ||
-      nextProps.format !== this.props.format
-    ) {
-      Environment.setBackgroundImage(nextProps.uri, {format: nextProps.format});
-    }
-  }
-
+export default class ReactVRPlayground extends React.Component {
   render() {
-    return null;
-  }
-}
-
-class Slideshow extends React.Component {
-  state = {
-    index: 0,
-  };
-
-  _prevPhoto = () => {
-    let next = this.state.index - 1;
-    if (next < 0) {
-      next += this.props.photos.length;
-    }
-    this.setState({
-      index: next,
-    });
-  };
-
-  _nextPhoto = () => {
-    this.setState({
-      index: this.state.index + 1,
-    });
-  };
-
-  render() {
-    const current = this.props.photos[
-      this.state.index % this.props.photos.length
-    ];
     return (
-      <View style={styles.wrapper}>
-        <Background uri={current.uri} format={current.format} />
-        <View style={styles.controls}>
-          <VrButton onClick={this._prevPhoto} style={styles.button}>
-            <Text style={styles.buttonText}>{'<'}</Text>
-          </VrButton>
-          <View>
-            <Text style={styles.title}>{current.title}</Text>
-          </View>
-          <VrButton onClick={this._nextPhoto} style={styles.button}>
-            <Text style={styles.buttonText}>{'>'}</Text>
-          </VrButton>
+      <View style={ styles.panel }>
+        <View style={ styles.greetingBox }>
+          <Text style={ styles.greeting }>
+            Welcome to React 360
+          </Text>
         </View>
       </View>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 600,
+  panel: {
+    // Fill the entire surface
     width: 1000,
-  },
-  controls: {
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
     alignItems: 'center',
-    width: 600,
-    padding: 10,
   },
-  title: {
-    color: '#ffffff',
-    textAlign: 'left',
-    fontSize: 36,
-    fontWeight: 'bold',
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
   },
-  button: {
-    backgroundColor: '#c0c0d0',
-    borderRadius: 5,
-    width: 40,
-    height: 44,
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: '#000000',
+  greeting: {
     fontSize: 30,
-    fontWeight: 'bold',
   },
 });
 
-AppRegistry.registerComponent('SlideshowSample', () => Slideshow);
+AppRegistry.registerComponent('ReactVRPlayground', () => ReactVRPlayground);
